65bc5f4fe9fff34200fb249552894c15c6816066
Aman <amanclementaranha@gmail.com>
2023-12-04 16:19:31 +0530

    rm unny imports

--- readmeUpdater.py
+++ readmeUpdater.py
@@ -1,15 +1,6 @@
-
-import random
 import json
-import requests
-import os
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-import pandas as pd
-import time
 
 driver = webdriver.Edge()
 
